8e5e5887de0d69d124242ff01450f29ceb90969b
Ola Taube <ola.taube@rebtel.com>
2017-03-14 09:52:37 +0100

    Ignored temp and dist folders. Included lib to handle jpegs

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,7 @@
 module.exports = function(grunt) {
 	'use strict';
 
-	// var mozjpeg = require('imagemin-mozjpeg');
+	var mozjpeg = require('imagemin-mozjpeg');
 
 	grunt.initConfig({
 		imagemin: {
